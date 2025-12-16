73b706af095d92a7e555c62f9e2dc182e3629ba2
cboi10 <choeyingjr11@gmail.com>
2025-12-16 16:30:58 +0530

    ready for deployment

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    country_code VARCHAR(2) NOT NULL,
+    country_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE visited_countries (
+    id SERIAL PRIMARY KEY,
+    country_code VARCHAR(2) NOT NULL UNIQUE
